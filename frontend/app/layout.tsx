c2c5cd45429370d05550a256865ea73228f0a87d
Elvis Kerebi <kerebipreye@gmail.com>
2025-08-21 12:48:39 +0200

    frontend setup

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Notely",
+  description:
+    "Notely is a note-taking app that allows you to take notes and share them with others.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        suppressHydrationWarning
+        className={`${inter.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
